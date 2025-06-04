dfd100c903088b177186150e19577dbb98fd2711
KamiSama <1457434343@qq.com>
2025-06-04 14:47:19 +0800

    update

--- Form1.cs
+++ Form1.cs
@@ -12,14 +12,40 @@ namespace VoiceHelper
 {
     public partial class Form1 : Form
     {
+        VoiceUtils voiceUtils = VoiceUtils.Instance;
         public Form1()
         {
             // 启动SocketServer（只需访问一次Instance即可自动启动）
             var server = VoiceHelper.SocketServer.Instance;
             server.OnMessageReceived += (clientId, message) =>
             {
-                // 这里可以处理收到的消息，比如输出到日志
-                Console.WriteLine($"收到客户端[{clientId}]消息: {message}");
+                this.Invoke(new Action(() =>
+                {
+                    int deviceIndex = this.comboBox1.SelectedIndex;
+
+                    if (message == "start")
+                    {
+                        voiceUtils.StartRecord(deviceIndex, 16000, 2);
+                    }
+                    else if (message == "stop")
+                    {
+                        voiceUtils.StopRecord();
+                    }
+
+                    Console.WriteLine($"收到客户端[{clientId}]消息: {message}");
+                }));
+            };
+
+            voiceUtils.OnRecording+= (channels) =>
+            {
+                this.Invoke(new Action(() =>
+                {
+                    // 处理录音数据
+                    foreach (var channel in channels)
+                    {
+                        Console.WriteLine($"Channel {channel.Channel}: Buffer Length = {channel.Buffer.Length}");
+                    }
+                }));
             };
             InitializeComponent();
         }
@@ -27,7 +53,7 @@ namespace VoiceHelper
         private void Form1_Load(object sender, EventArgs e)
         {
             
-            var devices = VoiceHelper.VoiceUtils.GetInputDevices();
+            var devices = voiceUtils.GetInputDevices();
 
             foreach (var device in devices)
             {
--- VoiceUtils.cs
+++ VoiceUtils.cs
@@ -1,16 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using NAudio.Wave;
 
 namespace VoiceHelper
 {
+    /// <summary>
+    /// 音频工具类，单例模式，支持实时录音
+    /// </summary>
     public class VoiceUtils
     {
+        private static readonly Lazy<VoiceUtils> _instance = new Lazy<VoiceUtils>(() => new VoiceUtils());
+        public static VoiceUtils Instance => _instance.Value;
+
+        private WaveInEvent _waveIn;
+        private bool _isRecording = false;
+        private int _channels = 1;
+
+        /// <summary>
+        /// 录音数据事件
+        /// 参数: channels: List<ChannelBuffer>，每个元素包含channel索引和buffer
+        /// </summary>
+        public event Action<List<ChannelBuffer>> OnRecording;
+
+        private VoiceUtils() { }
+
         /// <summary>
         /// 获取所有可用的音频输入设备（如麦克风）
         /// </summary>
         /// <returns>设备信息列表</returns>
-        public static List<AudioInputDevice> GetInputDevices()
+        public List<AudioInputDevice> GetInputDevices()
         {
             var devices = new List<AudioInputDevice>();
 
@@ -27,6 +46,105 @@ namespace VoiceHelper
 
             return devices;
         }
+
+        /// <summary>
+        /// 开始录音
+        /// </summary>
+        /// <param name="deviceIndex">设备索引</param>
+        /// <param name="sampleRate">采样率，默认16000</param>
+        /// <param name="channels">通道数，默认1</param>
+        public void StartRecord(int deviceIndex = 0, int sampleRate = 16000, int channels = 1)
+        {
+            if (_isRecording)
+                return;
+
+            _channels = channels;
+
+            _waveIn = new WaveInEvent
+            {
+                DeviceNumber = deviceIndex,
+                WaveFormat = new WaveFormat(sampleRate, channels)
+            };
+            _waveIn.DataAvailable += WaveIn_DataAvailable;
+            _waveIn.RecordingStopped += WaveIn_RecordingStopped;
+
+            _waveIn.StartRecording();
+            _isRecording = true;
+            Console.WriteLine("录音已开始");
+        }
+
+        /// <summary>
+        /// 停止录音
+        /// </summary>
+        public void StopRecord()
+        {
+            if (!_isRecording)
+                return;
+
+            _waveIn.StopRecording();
+            // 事件在RecordingStopped中处理
+        }
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            // 实时打印buffer长度
+            Console.WriteLine($"录音数据: {e.Buffer.Length} 字节, 有效数据: {e.BytesRecorded} 字节");
+
+            List<ChannelBuffer> channelBuffers = new List<ChannelBuffer>();
+
+            if (_channels == 2)
+            {
+                // 16位采样，每帧4字节（左2字节+右2字节）
+                int bytesPerSample = 2; // 16bit
+                int frameSize = bytesPerSample * 2; // 2通道
+                int frames = e.BytesRecorded / frameSize;
+                byte[] leftBuffer = new byte[frames * bytesPerSample];
+                byte[] rightBuffer = new byte[frames * bytesPerSample];
+
+                for (int i = 0; i < frames; i++)
+                {
+                    // 左声道
+                    leftBuffer[i * bytesPerSample] = e.Buffer[i * frameSize];
+                    leftBuffer[i * bytesPerSample + 1] = e.Buffer[i * frameSize + 1];
+                    // 右声道
+                    rightBuffer[i * bytesPerSample] = e.Buffer[i * frameSize + 2];
+                    rightBuffer[i * bytesPerSample + 1] = e.Buffer[i * frameSize + 3];
+                }
+
+                channelBuffers.Add(new ChannelBuffer { Channel = 0, Buffer = leftBuffer });
+                channelBuffers.Add(new ChannelBuffer { Channel = 1, Buffer = rightBuffer });
+            }
+            else if (_channels == 1)
+            {
+                // 单声道直接返回
+                byte[] monoBuffer = new byte[e.BytesRecorded];
+                Array.Copy(e.Buffer, 0, monoBuffer, 0, e.BytesRecorded);
+                channelBuffers.Add(new ChannelBuffer { Channel = 0, Buffer = monoBuffer });
+            }
+
+            // 触发事件
+            OnRecording?.Invoke(channelBuffers);
+        }
+
+        private void WaveIn_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            _waveIn.DataAvailable -= WaveIn_DataAvailable;
+            _waveIn.RecordingStopped -= WaveIn_RecordingStopped;
+            _waveIn.Dispose();
+            _waveIn = null;
+
+            Console.WriteLine("录音已停止");
+            _isRecording = false;
+        }
+    }
+
+    /// <summary>
+    /// 单个声道的buffer数据
+    /// </summary>
+    public class ChannelBuffer
+    {
+        public int Channel { get; set; }
+        public byte[] Buffer { get; set; }
     }
 
     /// <summary>
